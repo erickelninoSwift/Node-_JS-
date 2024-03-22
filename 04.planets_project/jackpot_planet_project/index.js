5c7c1d144f82f6f057fd8728ee20bb0444f3a7bb
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-23 01:30:39 +0200

    update

--- 04.planets_project/jackpot_planet_project/index.js
+++ 04.planets_project/jackpot_planet_project/index.js
@@ -2,6 +2,14 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 let records = [];
 
-fs.createReadStream("kepler_data.csv").on("data", (data) => {
-  records.push(data);
-});
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data) => {
+    return records.push(data);
+  })
+  .on("end", () => {
+    console.log("data was fetched succesfully");
+    console.log(records);
+  })
+  .on("error", (error) => {
+    console.log("error was foudn ", error);
+  });
