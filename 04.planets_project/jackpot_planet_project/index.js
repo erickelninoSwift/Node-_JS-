756b6445d12e77901edd671f7f636b9e083590b6
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-23 08:13:59 +0200

    update

--- 04.planets_project/jackpot_planet_project/index.js
+++ 04.planets_project/jackpot_planet_project/index.js
@@ -1,14 +1,36 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
-let records = [];
+let habitablePlanets = [];
 
+const isHabitable = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    return records.push(data);
+    if (isHabitable(data)) {
+      return habitablePlanets.push(data);
+    }
   })
   .on("end", () => {
-    console.log("data was fetched succesfully");
-    console.log(records);
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet.kepler_name;
+      })
+    );
+    console.log(
+      `The amount of planets habitable found : ${habitablePlanets.length}`
+    );
   })
   .on("error", (error) => {
     console.log("error was foudn ", error);
